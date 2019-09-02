dd362420e7f0e9c24fe59b95a8761b0526260bcc
WEI <tw9202@163.com>
2019-09-02 09:26:17 +0800

    Update FBP_algorithm.m

--- FBP_algorithm.m
+++ FBP_algorithm.m
@@ -1,21 +1,27 @@
-function [FBP_result] = FBP_algorithm(test_slice,pro_direction,globalstruct)
-%FBP_ALGORITHM 此处显示有关此函数的摘要
-%   此处显示详细说明
-    pro_name=[pro_direction,num2str(test_slice),'_pro.mat'];
+function [FBP_result_direction] = FBP_algorithm(pro_direction,globalstruct)
+%FBP_ALGORITHM 姝ゅ鏄剧ず鏈夊叧姝ゅ嚱鏁扮殑鎽樿
+%   姝ゅ鏄剧ず璇︾粏璇存槑
+warning off;
+mkdir('Result');
+cd('./Result');
+mkdir('FBP_result');
+cd('../')
+for slice=1:1:globalstruct.resolution
+    cd(pro_direction);
+    pro_name=[num2str(slice),'_pro.mat'];
     load(pro_name);
-
-
     resolution=globalstruct.resolution;
     thetaresolution=globalstruct.thetaresolution;
-
-    
     [iradon_x,iradon_y]=size(radon(ones(resolution,resolution),1:thetaresolution:180));
     data_iradon=[];
     data_iradon=[zeros(floor((iradon_x-size(pro,2))/2),size(pro,1));pro'];
     data_iradon=[data_iradon;zeros(iradon_x-size(pro,2)-floor((iradon_x-size(pro,2))/2),size(pro,1))];
     FBP_result=iradon(data_iradon,90:thetaresolution:269);
     FBP_result=FBP_result(2:resolution+1,2:resolution+1);
-
-
+    cd('../../Result/FBP_result');
+    save(['FBPresult_',num2str(slice),'.mat'],'FBP_result');
+    cd('../../');
+end
+FBP_result_direction='./Result/FBP_result/';
 end
 
